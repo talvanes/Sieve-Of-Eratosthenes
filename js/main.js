e4eb35d7beb2e8b3ec9b3c0326e6145435a3bd82
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2015-05-02 20:58:58 -0300

    The final Javascript code

--- js/main.js
+++ js/main.js
@@ -1,15 +1,38 @@
 $(document).ready(function(){
 	/* Sieve Of Eratosthenes */
 	// 1) Generate an array [from 0] until the number given
-	var array = [];
+	var primes = [];
 	var number = parseInt($('#number').val());
 	for (var i = 0; i <= number; i += 1){
-		if(i < 2) {array.push(false)}	// i=0 or i=1 are not primes...
-		else {array.push(true)}				// ...but the other ones can be
+		if(i < 2) {primes.push(false)}	// i=0 or i=1 are not primes...
+		else {primes.push(true)}				// ...but the other ones can be
 	}
 
-	// 2) Let's do the 'test' (it's an event fired from the button 'Go')
-	$(':button').click(function(){
+	// 2) Let's do the 'test' after clicking the button
+	$("button").on("click", function(){
+		// Doing the 'test'
+		for(var test = 2; test <= Math.ceil(Math.sqrt(number)); test += 1){
+			if(primes[test]){
+				//
+				for(var index = test + 1; index <= number; index += 1){
+					// checking divisibility among numbers
+					if(index % test == 0){
+						// if index divides a test number, it's not prime
+						primes[index] = false;
+					}
+				}
+			}
+		}
 
+		// Clean output
+		$('#result').empty();
+
+		// Traversing the resulting array
+		for(var x in primes){
+			// Show only prime numbers
+			if(primes[x]){
+				$('#result').append('<span class="number">' + x + '</span>');
+			}
+		}
 	});
 });
